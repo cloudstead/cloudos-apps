5a1f0868e9001fcd4794c58f58c7f6eb984d6e85
jonathan <jonathan@kyuss.org>
2014-10-03 08:56:20 -0700

    send consolidate output to stderr to avoid breaking prep.sh

--- prep-deploy.sh
+++ prep-deploy.sh
@@ -2,4 +2,4 @@
 
 BASE=$(cd $(dirname $0) && pwd)
 
-../cloudos-appstore/bin/cbundle ${BASE}/apps && ${BASE}/consolidate-cookbooks.sh
+../cloudos-appstore/bin/cbundle ${BASE}/apps && ${BASE}/consolidate-cookbooks.sh 1>&2
