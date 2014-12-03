91a2edf7ad2da6350a6aeb705d1cea012ad41122
jonathan <jonathan@kyuss.org>
2014-12-02 18:29:59 -0800

    sync apps during validation

--- apps/cloudos/recipes/validate.rb
+++ apps/cloudos/recipes/validate.rb
@@ -5,8 +5,15 @@
 # Copyright 2014, cloudstead
 #
 
+bash 'synchronize cloudos apps' do
+  user 'root'
+  code <<-EOF
+cos sync-apps --exports ~cloudos/.cloudos.env
+  EOF
+end
+
 bash 'ensure all services are running' do
-  user "root"
+  user 'root'
   code <<-EOF
 
 if [ $(netstat -nlpt | grep dovecot | wc -c) -eq 0 ] ; then
