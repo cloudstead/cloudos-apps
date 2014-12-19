434d007cf5c03a2ef8fd2ef84db29c092fc2489c
jonathan <jonathan@kyuss.org>
2014-12-19 15:23:09 -0800

    refactoring to support backup/restore

--- apps/cloudos/recipes/validate.rb
+++ apps/cloudos/recipes/validate.rb
@@ -16,8 +16,11 @@ bash 'ensure all services are running' do
   user 'root'
   code <<-EOF
 
-if [ $(netstat -nlpt | grep dovecot | wc -c) -eq 0 ] ; then
+if [ $(netstat -nlpt | grep '/dovecot' | wc -c) -eq 0 ] ; then
   service dovecot start
+fi
+if [ $(netstat -nlpt | grep '/master' | grep ':25' | wc -c) -eq 0 ] ; then
+  service postfix restart
 fi
 
   EOF
