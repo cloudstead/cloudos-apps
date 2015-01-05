bd5e3b678473b35bec19a27c025533e51823ca49
bryan schulte <bryan.schulte@gmail.com>
2015-01-05 10:23:45 -0800

    removing config.ldif from ldap restore

--- apps/auth/files/pre_restore.sh
+++ apps/auth/files/pre_restore.sh
@@ -10,5 +10,5 @@ if [ $(service slapd status | grep running | wc -l) -gt 0 ] ; then
   service slapd stop
 fi
 
-slapadd -F /etc/ldap/slapd.d -n 0 -l ${backup_dir}/config.ldif
+# slapadd -F /etc/ldap/slapd.d -n 0 -l ${backup_dir}/config.ldif
 slapadd -F /etc/ldap/slapd.d -n 1 -l ${backup_dir}/data.ldif
