8500831505202440b8217c68c8488ba7aa01a47f
jonathan <jonathan@kyuss.org>
2015-02-05 21:05:22 -0800

    single-signon now working with wordpress

--- apps/wordpress/files/install_cos_auth_plugin.php
+++ apps/wordpress/files/install_cos_auth_plugin.php
@@ -6,6 +6,16 @@ $base = $argv[1];
 require_once( $base . "/wp-load.php" );
 require_once(  $base . "/wp-admin/includes/plugin.php");
 
-activate_plugin($base . "wp-content/plugins/cos_auth.php");
+$plugin_path = $base . "/wp-content/plugins/cos_auth.php";
+
+$rval = activate_plugin($plugin_path);
+if (null == $rval) {
+    fwrite(STDOUT, "successfully activated plugin: $plugin_path\n");
+    exit(0);
+
+} else {
+    fwrite(STDERR, "error activating plugin: " . print_r($rval, 1) . "\n");
+    exit(1);
+}
 
 ?>
