5efe3094bf4e56e916ba1ad6fc4479da949d85ee
jonathan <jonathan@kyuss.org>
2015-02-21 18:15:57 -0800

    fix alias management

--- apps/email/libraries/email_lib.rb
+++ apps/email/libraries/email_lib.rb
@@ -1,3 +1,4 @@
+require 'securerandom'
 
 class Chef::Recipe::Email
 
@@ -5,11 +6,16 @@ class Chef::Recipe::Email
     '/etc/postfix/vmailbox'
   end
 
+  def self.sha_file(file)
+    Digest::SHA256.file(file).hexdigest
+  end
+
   def self.create_mailbox(chef, account, domain, deliver_to_account = nil)
 
     email_lib = Chef::Recipe::Email
     vmailbox = email_lib.vmailbox
     deliver_to_account ||= account
+    is_alias = (deliver_to_account != account)
 
     chef.bash "create email account for #{account} in email vhost #{domain}" do
       user 'root'
@@ -30,6 +36,14 @@ echo "#{account}@#{domain}   #{domain}/#{deliver_to_account}/Maildir/" >> #{vmai
       action :create
     end
 
+    if is_alias
+      chef.bash "create maildir symlink for #{account} -> #{deliver_to_account}" do
+        user 'root'
+        code <<-EOF
+ln -s /var/vmail/vhosts/#{domain}/#{deliver_to_account} /var/vmail/vhosts/#{domain}/#{account}
+        EOF
+      end
+    end
 # Pretty sure we don't need to do this, but leaving commented out for now
 #     chef.bash "create mailbox for user #{account} in /var/vmail/vhosts/#{domain}/#{deliver_to_account}" do
 #       user 'root'
@@ -39,20 +53,39 @@ echo "#{account}@#{domain}   #{domain}/#{deliver_to_account}/Maildir/" >> #{vmai
 #       EOF
 #     end
 
-    chef.bash 'compile virtual and vmailbox into postfix db files' do
+    chef.bash "remap vmailbox file #{vmailbox} (sha #{sha_file vmailbox})" do
       user 'root'
       code <<-EOF
 postmap #{vmailbox}
       EOF
     end
 
-    # ensure users are in vmailbox.users file
-    chef.bash 'compile virtual and vmailbox into postfix db files' do
-      user 'root'
-      code <<-EOF
-echo #{account} >> #{vmailbox}.users
-      EOF
-      not_if { %x(cat #{vmailbox}.users | grep #{account} | wc -l | tr -d '').strip.to_i > 0 }
+    if is_alias
+      # ensure aliases are in virtual file
+      virtual_file='/etc/postfix/virtual'
+      chef.bash "add email alias #{account} to #{vmailbox}.alias" do
+        user 'root'
+        code <<-EOF
+  echo "
+#{account}  #{deliver_to_account}
+" >> #{virtual_file}  && \
+sed -e -i '/^ *$/d' #{virtual_file}  # strip blank lines
+        EOF
+        not_if { %x(cat #{virtual_file} | grep #{account} | wc -l | tr -d '').strip.to_i > 0 }
+      end
+
+    else
+      # ensure users are in vmailbox.users file
+      chef.bash "add email account #{account} to #{vmailbox}.users" do
+        user 'root'
+        code <<-EOF
+  echo "
+#{account}
+" >> #{vmailbox}.users && \
+sed -e -i '/^ *$/d' #{vmailbox}.users  # strip blank lines
+        EOF
+        not_if { %x(cat #{vmailbox}.users | grep #{account} | wc -l | tr -d '').strip.to_i > 0 }
+      end
     end
 
     # for some reason this directory ends up being owned by root (?)
