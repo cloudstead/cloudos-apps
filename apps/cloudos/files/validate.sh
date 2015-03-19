c4004bf33db5f5887cb8b6c19bf9f7e31c377d2c
jonathan <jonathan@kyuss.org>
2015-03-18 20:17:03 -0700

    refactor validation. add server-side email filtering via sieve

--- apps/cloudos/files/validate.sh
+++ apps/cloudos/files/validate.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [ $(netstat -nlpt | grep '/dovecot' | wc -c) -eq 0 ] ; then
+if [ $(netstat -nlpt | grep '/dovecot' | grep :143 | wc -l) -eq 0 ] ; then
   service dovecot start
 fi
-if [ $(netstat -nlpt | grep '/master' | grep ':25' | wc -c) -eq 0 ] ; then
+if [ $(netstat -nlpt | grep '/master' | grep ':25' | wc -l) -eq 0 ] ; then
   service postfix restart
 fi
 if [ $(service cloudos status | grep "is running" | wc -l) -eq 0 ] ; then
