a60da8082ea8016560576e5883f78e8fe4c183ba
jonathan <jonathan@kyuss.org>
2015-09-02 16:36:39 -0700

    setup rooty if needed. add cdns command

---
+++ apps/cloudos-dns/files/setup_rooty_if_djbdns.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BASE=$(cd $(dirname $0) && pwd)
+SERVER_TYPE="${1:?no server_type specified}"
+
+INIT=cloudos-dns-rooty
+ETC_INIT="/etc/init.d/${INIT}"
+
+if [[ ${SERVER_TYPE} == "djbdns" && ! -f ${ETC_INIT} ]] ; then
+
+  cp "${BASE}/${INIT}" ${ETC_INIT}
+  chmod 700 ${ETC_INIT}
+
+  service cloudos-dns-rooty restart
+
+fi
