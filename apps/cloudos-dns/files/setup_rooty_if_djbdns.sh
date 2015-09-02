ec0efc938e40119c4b0ba89dd98155707bb2af4f
jonathan <jonathan@kyuss.org>
2015-09-02 16:58:46 -0700

    always copy init file and restart rooty

--- apps/cloudos-dns/files/setup_rooty_if_djbdns.sh
+++ apps/cloudos-dns/files/setup_rooty_if_djbdns.sh
@@ -6,11 +6,8 @@ SERVER_TYPE="${1:?no server_type specified}"
 INIT=cloudos-dns-rooty
 ETC_INIT="/etc/init.d/${INIT}"
 
-if [[ ${SERVER_TYPE} == "djbdns" && ! -f ${ETC_INIT} ]] ; then
-
+if [[ ${SERVER_TYPE} == "djbdns" ]] ; then
   cp "${BASE}/${INIT}" ${ETC_INIT}
   chmod 700 ${ETC_INIT}
-
   service cloudos-dns-rooty restart
-
 fi
