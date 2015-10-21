7060915cca03f0ee1014992f6e911a2f3151d2ed
jonathan <jonathan@kyuss.org>
2015-10-20 19:18:34 -0700

    add support for locking down access via HTTP Digest auth to a single super-user login

---
+++ apps/apache-root/recipes/validate.rb
@@ -0,0 +1,9 @@
+#
+# Cookbook Name:: apache-root
+# Recipe:: validate
+#
+# Copyright 2015 Cloudstead, Inc.
+#
+
+# noop for now
+# todo: validation rule: if HTTP auth is disabled, we expect a 200 response. If auth is enabled, we expect a 401 response.
