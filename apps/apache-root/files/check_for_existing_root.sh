16cb8bb92116afa665e121ed662fc8529eb147c1
jonathan <jonathan@kyuss.org>
2015-10-26 15:07:04 -0700

    allow mixin to be turned on/off, better support for non-https sites

--- apps/apache-root/files/check_for_existing_root.sh
+++ apps/apache-root/files/check_for_existing_root.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ENABLED=/etc/apache2/sites-enabled
-roots="$(find ${ENABLED} | egrep -v "sites-enabled$" | xargs -n 1 egrep -l -- '<VirtualHost\s+.+:443\s*>' | xargs egrep -l "^\s*ServerName\s*$(hostname)\s*")"
+roots="$(find ${ENABLED} | egrep -v "sites-enabled$" | xargs -n 1 egrep -l -- '<VirtualHost\s+.+:[[:digit:]]+\s*>' | xargs egrep -l "^\s*ServerName\s*$(hostname)\s*")"
 
 for config in ${roots} ; do
   if [ $(basename ${config}) != "apache-root.conf" ] ; then
