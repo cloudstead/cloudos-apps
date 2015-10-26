e47015203077fb30bd35da6922f5da417c24070d
jonathan <jonathan@kyuss.org>
2015-10-26 15:03:17 -0700

    allow piwik mixin to be turned on/off

--- apps/piwik/files/piwik_user_add.sh
+++ apps/piwik/files/piwik_user_add.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+function die {
+  echo 1>&2 "${1}"
+  exit 1
+}
+
 DBNAME="${1}"
 LOGIN="${2}"
 PASSWORD=$(echo -n "${3}" | md5sum | awk '{print $1}')
@@ -7,12 +12,14 @@ TOKEN_AUTH=$(echo -n "${LOGIN}${PASSWORD}" | md5sum | awk '{print $1}')
 EMAIL="${4}"
 SUPERUSER="${5}"
 
-found=$(echo "select count(*) from piwik_user where login='"${LOGIN}"'" | mysql -s -u root ${DBNAME} | tr -d ' ')
+MYSQL="mysql --defaults-file=$(cd ~root && pwd)/.my.cnf -s -u root ${DBNAME}"
+
+found=$(echo "select count(*) from piwik_user where login='"${LOGIN}"'" | ${MYSQL} | tr -d ' ') || die "Error looking up user in piwik database"
 if [[ ! -z "${found}" && ${found} -eq 0 ]] ; then
   echo "insert into piwik_user (login, password, alias, email, token_auth, superuser_access, date_registered)
         values ('"${LOGIN}"', '"${PASSWORD}"', '"${LOGIN}"', '"${EMAIL}"', '"${TOKEN_AUTH}"',
         ${SUPERUSER}, '"$(date "+%Y-%m-%d %H:%M:%S")"');" \
-  | mysql -u root ${DBNAME}
+  | ${MYSQL} || die "Error adding mysql user to piwik database: ${LOGIN}"
 else
   echo "user already exists: ${LOGIN}"
 fi
