f3d26cdd3c2c95bb6d5ad34263a9d42dcdff5c83
jonathan <jonathan@kyuss.org>
2015-10-29 12:20:09 -0700

    ensure pgsql is running

--- apps/postgresql/recipes/default.rb
+++ apps/postgresql/recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: postgresql
 # Recipe:: default
 #
-# Copyright 2013, cloudstead
+# Copyright 2013-2015, cloudstead
 #
 # All rights reserved - Do Not Redistribute
 #
@@ -11,4 +11,18 @@
   package pkg do
     action :install
   end
+end
+
+bash 'ensure postgresql is running' do
+  user 'root'
+  code <<-EOF
+status="$(service postgresql status)"
+if [ -z "${status}" ] ; then
+  echo "Error getting postgresql service status"
+  exit 1
+fi
+if [[ ! "${status}" =~ "online" ]] ; then
+  service postgresql restart
+fi
+  EOF
 end
